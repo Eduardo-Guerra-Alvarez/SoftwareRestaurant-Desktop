e50a2c402f27541f3050c29ff51d52aef18e6c04
Eduardo-Guerra-Alvarez <lalo52rojo@gmail.com>
2025-10-27 19:54:32 -0600

    implementation view details

--- src/main/java/com/eduardo/softwarerestaurantdesktop/controllerFX/OrderSelectedControllerFX.java
+++ src/main/java/com/eduardo/softwarerestaurantdesktop/controllerFX/OrderSelectedControllerFX.java
@@ -1,9 +1,16 @@
 package com.eduardo.softwarerestaurantdesktop.controllerFX;
 
+import com.eduardo.softwarerestaurantdesktop.api.ApiServiceOrderDetail;
 import com.eduardo.softwarerestaurantdesktop.dao.OrderDAO;
+import com.eduardo.softwarerestaurantdesktop.dao.OrderDetailsDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,10 +26,27 @@ public class OrderSelectedControllerFX implements Initializable {
     private Label totalOrder;
     @FXML
     private Label statusOrder;
+    @FXML
+    private TableView<OrderDetailsDAO> tableViewOrderDetail;
+    @FXML
+    private TableColumn<OrderDetailsDAO, Long> idCol;
+    @FXML
+    private TableColumn<OrderDetailsDAO, String> menuCol;
+    @FXML
+    private TableColumn<OrderDetailsDAO, Integer> quantityCol;
+    @FXML
+    private TableColumn<OrderDetailsDAO, Float> unitPriceCol;
+    @FXML
+    private TableColumn<OrderDetailsDAO, Float> subtotalCol;
+    @FXML
+    private TableColumn<OrderDetailsDAO, Void> actionCol;
+
+    private final ObservableList<OrderDetailsDAO> orderDetailsList = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        setColumns();
+        tableViewOrderDetail.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
     }
 
     public void setOrder(OrderDAO order) {
@@ -31,5 +55,48 @@ public class OrderSelectedControllerFX implements Initializable {
         dateOrder.setText(order.getCreated_at());
         totalOrder.setText("Total: " + order.getTotal());
         statusOrder.setText(order.getStatus());
+        orderDetailsList.setAll(order.getOrderDetails());
+        tableViewOrderDetail.setItems(orderDetailsList);
+    }
+
+    private void setColumns() {
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        menuCol.setCellValueFactory(new PropertyValueFactory<>("menu"));
+        quantityCol.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        unitPriceCol.setCellValueFactory(new PropertyValueFactory<>("unit_price"));
+        subtotalCol.setCellValueFactory(new PropertyValueFactory<>("subtotal"));
+        addDeleteButtonToTable();
+    }
+
+    private void addDeleteButtonToTable() {
+        actionCol.setCellFactory(col -> new TableCell<>() {
+            private final Button deleteButton = new Button("Eliminar");
+
+            {
+                deleteButton.getStyleClass().add("buttonRed");
+
+                deleteButton.setOnAction(event -> {
+                    OrderDetailsDAO item = getTableView().getItems().get(getIndex());
+                    deleteItem(item.getId());
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    HBox box = new HBox(deleteButton);
+                    box.setAlignment(Pos.CENTER);
+                    setGraphic(box);
+                }
+            }
+        });
+    }
+
+    private void deleteItem(Long orderDetailId) {
+        String status = ApiServiceOrderDetail.deleteOrderDetail(orderDetailId);
+        System.out.println(status);
     }
 }
